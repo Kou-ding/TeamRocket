f061a89488af750a8ad548c238e686d7b77a2336
Kou <fotispapadak@gmail.com>
2024-11-19 15:25:52 +0200

    backend example test

--- tests/jamesbackend.test.js
+++ tests/jamesbackend.test.js
@@ -8,7 +8,6 @@ test.before(async (t) => {
 	const server = t.context.server.listen();
     const { port } = server.address();
 	t.context.got = got.extend({
-		responseType: "json", 
 		prefixUrl: `http://localhost:${port}`
 	});
 });
@@ -19,11 +18,13 @@ test.after.always((t) => {
 
 // Test the /docs endpoint
 test("GET /docs should return status 200", async (t) => {
-	const response = await t.context.got('docs');  // equivalent to: http://localhost:8080/docs
-	// Log the full response
-	console.log('Response Body:', response.body);  // Prints the body of the response
-	console.log('Response Status Code:', response.statusCode);  // Prints the status code
-	console.log('Response Headers:', response.headers);  // Prints the headers
-	//const { statusCode } = await t.context.got("docs");
-	t.is(statusCode, 200, "Expected a 200 status code");
+	const response = await t.context.got('docs/');  // equivalent to: http://localhost:8080/docs
+    // Log the full response
+	// console.log('Response Body:', response.body);  // Prints the body of the response
+	// console.log('Response Status Code:', response.statusCode);  // Prints the status code
+	// console.log('Response Headers:', response.headers);  // Prints the headers
+    t.is(response.statusCode, 200, "Expected a 200 status code");
+    t.is(response.headers["content-type"], "text/html; charset=UTF-8", "Expected a text/html content type");
+    t.is(response.headers["swagger-api-docs-url"], "/api-docs", "Expected a /api-docs URL");
+    t.is(response.body.includes("Swagger UI"), true, "Expected the response body to include 'Swagger UI'");
 });
